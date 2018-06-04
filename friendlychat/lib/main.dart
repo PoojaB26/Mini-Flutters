f6eef61f7c746226440fce4b1248577fd9f36faf
PoojaB26 <pbhaumik26@gmail.com>
2018-06-04 12:53:46 +0530

    Responsive message field

--- friendlychat/lib/main.dart
+++ friendlychat/lib/main.dart
@@ -14,15 +14,48 @@ class FriendlyChat extends StatelessWidget{
   }
 }
 
-class ChatScreen extends StatelessWidget{
+class ChatScreen extends StatefulWidget{
+  @override
+  State createState() => new ChatScreenState();
+}
+
+class ChatScreenState extends State<ChatScreen>{
+  final TextEditingController _textController = new TextEditingController();
   @override
   Widget build(BuildContext context) {
    return new Scaffold(
       appBar: new AppBar(
         title: new Text("Friendly Chat AppBar"),
-
       ),
+     body: _buildTextComposer(), //new
 
-    ); }
+   ); }
 
+  void _handleSubmitted(String text) {
+    _textController.clear();
+  }
+
+  Widget _buildTextComposer() {
+    return new Container(
+      margin: const EdgeInsets.symmetric(horizontal: 8.0),
+      child: new Row(
+        children: <Widget>[
+          new Flexible(child: new TextField(
+            controller: _textController,
+            onSubmitted: _handleSubmitted,
+            decoration: new InputDecoration.collapsed(
+                hintText: "Send a message"),
+          ),
+          ),
+          new Container(
+            margin: new EdgeInsets.symmetric(horizontal: 4.0),
+            child: new IconButton(
+                icon: new Icon(Icons.send),
+                onPressed:() => _handleSubmitted(_textController.text))
+          )
+        ],
+      )
+    );
+  }
 }
+
