2a83a63ee447a19c8cb9c1b9664110cb070edfc7
PoojaB26 <pbhaumik26@gmail.com>
2018-06-06 02:27:13 +0530

    Added content to the lists

--- treva_mock/lib/ui/home/HomePageBody.dart
+++ treva_mock/lib/ui/home/HomePageBody.dart
@@ -1,9 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:treva_mock/ui/home/PlanetRow.dart';
+import 'package:treva_mock/model/Planet.dart';
 
 class HomePageBody extends StatelessWidget{
   @override
   Widget build(BuildContext context){
-    return new PlanetRow();
+    return new PlanetRow(planets[0]);
   }
 }
